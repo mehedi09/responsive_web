3ca122a76eee48ae9e72f27ce85a2f82abb59bef
mehedi09 <mehedi_aiub@hotmail.com>
2014-08-19 19:08:02 +0600

    edit readme and add new hello.html

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>First class at BASIS</title>
+        <link type="">
+		
+		</link>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <style type="text/css">
+			li:last-child
+			{
+				background-color: red;
+			}
+			input[type="email"]	
+			{
+				border: 1px solid red;
+			}
+			b+i { background-color: red;
+			}
+			b~i { background-color: green;
+		}
+
+
+@media all and (max-width: 400px) {
+  	body {
+  			background-color:red;}
+}
+
+@media (min-width: 401px) and (max-width: 600px){
+  	body {
+  			background-color:blue;}
+}
+@media (min-width: 601px)and (max-width:910px){
+  	body {
+  			background-color:green;}
+}
+
+
+
+
+</style>
+    <body>
+    	<h1>Welcome</h1><a href="http://www.w3.org/TR/css3-selectors/">Read IT</a>
+    	<a href="http://www.w3.org/TR/css3-selectors/">oRelly css specialyty and cascade</a>
+        rusdid@gmail.com
+        <ul>
+        	<li>Line 1</li>
+        	<li>Line 2</li>
+        	<li>Line 3</li>
+        	<li>Line 4</li>
+			<li>Line 5</li>
+        </ul>
+        <input type="text" placeholder="Enter your name">
+
+        <input type="email" placeholder="Enter your email">
+		<p>
+			<div>
+				<b>Hello i am a student of BASIS</b>
+						<i> I am Italic</i>
+				
+							<i> I am Italic 2nd
+							</i>
+			
+						<i> I am Italic 2nd</i>
+				
+					<i> I am Italic 3rd</i>
+			</div>
+		</p>
+				<p>
+					<div class="ss">
+						<h1>
+spacifity calculation
+						</h1>
+					</div>
+				</p>	
+    </body>
+</html>
